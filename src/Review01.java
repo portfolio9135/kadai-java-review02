a75b542f4e8a55393cf3869145a3c617cb46f077
portfolio9135 <ts.91358922@gmail.com>
2024-05-22 12:38:29 +0900

    再提出__Lesson3__課題__税込価格を計算しよう

---
+++ src/Review01.java
@@ -0,0 +1,14 @@
+
+public class Review01 {
+
+    public static void main(String[] args) {
+        int price = 1500;
+        double taxRate = 10;
+       
+        int tax = (int) (price * taxRate / 100);
+        int taxIncludedPrice = price + tax;
+        
+        System.out.println(price + "円の商品の税込価格は" + taxIncludedPrice + "円（消費税は" + tax + "円）です。");
+    }
+
+}
